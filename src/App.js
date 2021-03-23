b2e110e7df15129172019a18e6e8cb97ba95ad6d
deyizhang <deyizhang2003@gmail.com>
2021-03-23 02:24:53 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
-const App = () => (
+
+const App = () => {
+  console.log('Hello from the console.')
+  return(
   <div>
     <p>Hello world</p>
   </div>
-)
+  )
+}
 
 export default App
