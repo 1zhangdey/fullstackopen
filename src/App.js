a8bf252789c18fa41306943491d1392f1011d172
deyizhang <deyizhang2003@gmail.com>
2021-03-31 22:00:45 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
-const Hello = ({name,age}) => {
-  const bornYear = () =>{
-    const yearNow = new Date().getFullYear();
-    return(yearNow - age);
-  }
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>
-        This means you were probably born in {bornYear()}!
-      </p>
-    </div>
-  )
-}
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
-
+  const [counter, setCounter ] = useState(0);
+  
+  setTimeout(
+    () => setCounter(counter +1),
+    1000,
+  )
   return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+    <div>{counter}</div>
   )
 }
-
-
 export default App
