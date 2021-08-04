2fef1c5df9c35aab6b9654f264e2094b1a793d87
deyizhang <deyizhang2003@gmail.com>
2021-08-04 16:18:59 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
-import React, { useState } from "react";
+import React from 'react'
 
-const App = () => {
-  const [index, setIndex] = useState(0);
-  const [vote, setVote] = useState([0, 0, 0, 0, 0, 0]);
+const App = (props) => {
+  const { notes } = props
 
-  const anecdotes = [
-    "If it hurts, do it more often",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-  ];
-
-  const handleChange = () => {
-    setIndex(Math.floor(Math.random() * anecdotes.length));
-  };
-
-  const voteChange = () => {
-    let copy = [...vote];
-    copy[index]++;
-    setVote(copy);
-  };
-
-  const findMax = () => {
-    let max = -1,
-      maxKey = -1;
-
-    for (let key in vote) {
-      if (vote[key] > max) {
-        maxKey = key;
-        max = vote[key];
-      }
-    }
-    return maxKey;
-  };
-
-  console.log(findMax());
   return (
     <div>
-      <h1>Anecdote of the Day</h1>
-      <div>
-        {anecdotes[index]}
-        <br />
-        Has {vote[index]} Votes
-      </div>
-      <div>
-        <button onClick={handleChange}>next anecdote</button>
-        <button onClick={voteChange}>vote</button>
-      </div>
-      <h1>Anecdote of all Time!</h1>
-      <div>{anecdotes[findMax()]}</div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <li>{note.content}</li>)}
+      </ul>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
