4b9d552b05bd4c4fa4fbb1a3df6c3fdb6cff434d
deyizhang <deyizhang2003@gmail.com>
2021-08-06 19:02:32 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
+import Note from './components/Note';
 
-const App = (props) => {
-  const { notes } = props
-
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li>{note.content}</li>)}
+        {notes.map((note, i) => (
+        <li key = {i}>{note.content}</li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
